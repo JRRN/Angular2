1afb85f7d75476f9afb116f0efe7a4297762b8c9
jrafa.ramon <jrrn@MacBook-Pro-de-JRRN.local>
2017-07-25 20:02:02 +0200

    Quick Binding

--- Angular2DK/src/app/app.module.ts
+++ Angular2DK/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
